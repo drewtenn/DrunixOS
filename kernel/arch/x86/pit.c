190b3501cdc60652992c57cf356552827139f484
Drew Tennenbaum <drew@drew.dev>
2026-04-22 18:01:27 -0700

    kernel: clamp pit divisor range

--- kernel/arch/x86/pit.c
+++ kernel/arch/x86/pit.c
@@ -11,6 +11,8 @@
 #include <stdint.h>
 
 #define PIT_INPUT_HZ 1193182u
+#define PIT_MIN_DIVISOR 1u
+#define PIT_MAX_DIVISOR 65536u
 #define PIT_CMD_PORT 0x43
 #define PIT_CH0_PORT 0x40
 #define PIT_MODE_RATE_GENERATOR 0x34u
@@ -32,17 +34,22 @@ void pit_set_periodic_handler(pit_handler_fn fn)
 void pit_start(uint32_t hz)
 {
 	uint32_t divisor;
+	uint16_t reload_value;
 
 	if (hz == 0)
 		return;
 
 	divisor = PIT_INPUT_HZ / hz;
-	if (divisor == 0)
-		divisor = 1u;
+	if (divisor < PIT_MIN_DIVISOR)
+		divisor = PIT_MIN_DIVISOR;
+	else if (divisor > PIT_MAX_DIVISOR)
+		divisor = PIT_MAX_DIVISOR;
+
+	reload_value = (divisor == PIT_MAX_DIVISOR) ? 0u : (uint16_t)divisor;
 
 	port_byte_out(PIT_CMD_PORT, PIT_MODE_RATE_GENERATOR);
-	port_byte_out(PIT_CH0_PORT, (uint8_t)(divisor & 0xFFu));
-	port_byte_out(PIT_CH0_PORT, (uint8_t)((divisor >> 8) & 0xFFu));
+	port_byte_out(PIT_CH0_PORT, (uint8_t)(reload_value & 0xFFu));
+	port_byte_out(PIT_CH0_PORT, (uint8_t)((reload_value >> 8) & 0xFFu));
 }
 
 void pit_handle_irq(void)
