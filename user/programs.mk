da2bd17a7cfaa6085094348a8c75c8dcb0b61da4
Drew Tennenbaum <drew@drew.dev>
2026-04-27 18:58:48 -0700

    Convert crash to Rexy

--- user/programs.mk
+++ user/programs.mk
@@ -1,5 +1,5 @@
 PROGS = chello shell bbcompat dufstest redirtest ext3wtest threadtest tcccompat nanocompat terminal files processes help hello writer reader sleeper date which cat echo wc grep head tail tee sleep env printenv basename dirname cmp yes sort uniq cut kill crash dmesg cpphello lsblk desktop
 C_PROGS = chello shell bbcompat dufstest redirtest ext3wtest threadtest tcccompat nanocompat terminal files processes help desktop
-CXX_PROGS = crash lsblk
-REXC_PROGS ?= hello echo printenv cat yes basename dirname sleep cmp which head wc writer reader sleeper date grep tee dmesg tail kill cpphello uniq sort cut env
+CXX_PROGS = lsblk
+REXC_PROGS ?= hello echo printenv cat yes basename dirname sleep cmp which head wc writer reader sleeper date grep tee dmesg tail kill cpphello uniq sort cut env crash
 LINUX_PROGS =
